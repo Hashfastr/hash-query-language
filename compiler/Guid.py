f55a491b373ff5a0913a5c3ac51f38ccfe619501
Sylvain Jones <sylvainjones@hashfastr.com>
2025-04-20 13:37:04 -0600

    Commented guid

--- compiler/Guid.py
+++ compiler/Guid.py
@@ -1,6 +1,8 @@
 import random
 import hashlib
 
+# The GUIDs we use throughout the end generation
+# Are just a hash of the string of a random float, keeping the first 8 chars.
 def gen_guid():
     num = f'{random.random()}'.encode('ascii')
     hashobj = hashlib.sha256(num)
