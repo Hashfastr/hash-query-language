e33e73611b614958d314a3b5bbd0b275ddd4a5c7
Sylvain Jones <sylvainjones@hashfastr.com>
2025-05-08 20:10:04 -0600

    Basic extends work

--- HqlCompiler/Operators/Extend.py
+++ HqlCompiler/Operators/Extend.py
@@ -2,6 +2,8 @@ from .Operator import Operator
 from ..Results import Results
 from HqlCompiler.Expression import Expression
 from HqlCompiler.Exceptions import *
+from HqlCompiler.PolarsTools import PolarsTools
+import polars as pl
 
 # Creates a field with a value in the extend
 #
@@ -14,8 +16,29 @@ class Extend(Operator):
     def __init__(self, exprs:list[Expression]):
         super().__init__()
         self.exprs = exprs
-        
-    def execute(self, data:Results):
 
+    def extend(self, data:pl.DataFrame, src:list[str], dest:list[str]):
+        src_data = PolarsTools.get_element_series(data, src)
+        # Can do whatever with src_data here
+        dest_data = PolarsTools.build_element(dest, src_data)
+        return dest_data
+            
+    def execute(self, data:Results):
+        new = [data]
+        for i in self.exprs:
+            if i.type != "NamedExpression":
+                raise CompilerException(f'Extend operator given invalid expression {i.type}')
+            
+            if i.name.type == 'Path':
+                dest = [x.get_name() for x in i.name.path]
+            else:
+                dest = [i.name.get_name()]
+            
+            if i.value.type == 'Path':
+                src = [x.get_name() for x in i.value.path]
+            else:
+                raise CompilerException(f'Unhandled NamedExpression value type {i.value.type}')
+                
+            new.append(self.extend(data, src, dest))
         
-        return data
+        return pl.concat(new, how='horizontal')
