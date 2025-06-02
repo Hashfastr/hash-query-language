57dc2ab62f2a24e9645eae2a1f3267546af3cc7c
Sylvain Jones <sylvainjones@hashfastr.com>
2025-06-01 21:38:00 -0600

    Adding more functions really is that easy!!!

--- HqlCompiler/Functions/typecasting.py
+++ HqlCompiler/Functions/typecasting.py
@@ -4,15 +4,32 @@ from HqlCompiler.Data import Data
 from .__proto__ import Function
 from HqlCompiler.Types.Hql import HqlTypes as hqlt
 
-# This is a meta function resolved while parsing
-@register_func('toint')
-class toint(Function):
+class Typecast(Function):
     def __init__(self, args:list):
-        super().__init__(args, 1, 1,)
+        Function.__init__(self, args, 1, 1)
         self.src = args[0]
-                
+        # default cast type
+        self.cast_type = hqlt.string()
+        
     def eval(self, ctx:Context, **kwargs):        
         path = self.src.eval(ctx, as_list=True)
         data = ctx.data.select(path)
 
-        return data.cast_in_place(path, hqlt.int())
+        return data.cast_in_place(path, self.cast_type)
+@register_func('toint')
+class toint(Typecast):
+    def __init__(self, args:list):
+        Typecast.__init__(self, args)
+        self.cast_type = hqlt.int()
+
+@register_func('tofloat')
+class tofloat(Typecast):
+    def __init__(self, args:list):
+        Typecast.__init__(self, args)
+        self.cast_type = hqlt.float()
+        
+@register_func('todouble')
+class todouble(Typecast):
+    def __init__(self, args:list):
+        Typecast.__init__(self, args)
+        self.cast_type = hqlt.double()
--- tests/hql-operators/unnest/unnest.hql
+++ tests/hql-operators/unnest/unnest.hql
@@ -1,5 +1,5 @@
 database('json').file('./notebooks/so-network-data.json')
 | unnest _source on ['./notebooks/so-network-data.json']
 | project ['@version'], network.data.decoded
-| extend log_version=toint(['@version'])
+| extend log_version=tofloat(['@version'])
 | take 10
