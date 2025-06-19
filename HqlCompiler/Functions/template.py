516b3b06edde931a363fc1299bf7f9a06186fe41
Sylvain Jones <sylvainjones@hashfastr.com>
2025-06-18 21:40:02 -0600

    Added a function template

---
+++ HqlCompiler/Functions/template.py
@@ -0,0 +1,14 @@
+from HqlCompiler.Exceptions import *
+from HqlCompiler.Context import register_func, Context
+import logging
+from .__proto__ import Function
+from HqlCompiler.Data import Data, Series, Table, Schema
+
+# @register_func('template')
+class template(Function):
+    def __init__(self, args:list):
+        # allows 1 to infinity args
+        super().__init__(args, 1, -1)
+        
+    def eval(self, ctx:Context, **kwargs):
+        return Data()
