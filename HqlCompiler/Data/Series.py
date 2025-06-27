0bb78b5af7220bc5e61dcfc1ffe836dae0a48aed
Sylvain Jones <sylvainjones@hashfastr.com>
2025-06-27 17:24:48 -0600

    Fixed up Series

--- HqlCompiler/Data/Series.py
+++ HqlCompiler/Data/Series.py
@@ -1,10 +1,20 @@
 import polars as pl
 from HqlCompiler.Types.Hql import HqlTypes as hqlt
+from HqlCompiler.Types.Polars import PolarsTypes as plt
 
 '''
 Series for individual values, mimics a pl.Series
 '''
 class Series():
-    def __init__(self, series:pl.Series, stype:hqlt.HqlType):
+    def __init__(self, series:pl.Series=None, stype:hqlt.HqlType=None):
+        if stype == None and not isinstance(series, type(None)):
+            ptype = series.dtype
+            stype = plt.from_pure_polars(ptype)
+        
         self.series = series
         self.type = stype
+
+    def __bool__(self)-> bool:
+        if isinstance(self.series, type(None)):
+            return False
+        return True
