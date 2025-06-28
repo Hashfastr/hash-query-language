15ff32dcd9bfd6e84b82a7c550a3cdb6db8b442c
Sylvain Jones <sylvainjones@hashfastr.com>
2025-06-28 09:33:50 -0600

    Fixed up ip4subnet

--- HqlCompiler/Functions/ip4subnet.py
+++ HqlCompiler/Functions/ip4subnet.py
@@ -1,3 +1,4 @@
+from typing import overload
 from HqlCompiler.Exceptions import *
 from HqlCompiler.Context import register_func, Context
 import logging
@@ -11,14 +12,20 @@ from HqlCompiler.Expression import BasicRange
 class ip4subnet(Function):
     def __init__(self, args:list):
         super().__init__(args, 1, 1)
-        
+    
     def eval(self, ctx:Context, **kwargs):
         import re
-        subnet_regex = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\/(\d{1,2})'
+        subnet_regex = '(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\\/(\\d{1,2})'
         
         subnet_text = self.args[0].eval(ctx, as_str=True)
-        ip_text   = re.match(subnet_regex, subnet_text).group(1)
-        mask_text = re.match(subnet_regex, subnet_text).group(2)
+        ip_text   = re.match(subnet_regex, subnet_text)
+        mask_text = re.match(subnet_regex, subnet_text)
+
+        if ip_text == None or mask_text == None:
+            raise QueryException(f'Invalid subnet given {subnet_text}')
+
+        ip_text = ip_text.group(1)
+        mask_text = mask_text.group(2)
         
         mask = 0xFFFFFFFF - ((1 << (32 - int(mask_text))) - 1)        
         ip_int = hqlt.ip4().cast(pl.Series([ip_text]))[0]
