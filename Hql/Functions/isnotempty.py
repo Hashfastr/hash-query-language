5b8bee6f1a9a813e30fed8dd761b93ea48f98685
Sylvain Jones <sylvainjones@hashfastr.com>
2025-10-08 22:51:55 -0600

    Added isnotempty function

--- Hql/Functions/isnotempty.py
+++ Hql/Functions/isnotempty.py
@@ -2,14 +2,35 @@ from . import Function
 from Hql.Exceptions import HqlExceptions as hqle
 from Hql.Context import register_func, Context
 from Hql.Data import Data, Series, Table, Schema
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from Hql.Context import Context
 
 import logging
+import polars as pl
 
-# @register_func('template')
-class template(Function):
+@register_func('isnotempty')
+class isnotempty(Function):
     def __init__(self, args:list):
         # allows 1 to infinity args
         Function.__init__(self, args, 1, -1)
+
+    def gen_filter(self, ctx:'Context') -> pl.Expr:
+        expr:Optional[pl.Expr] = None
+
+        for i in self.args:
+            cur = i.eval(ctx, as_pl=True)
+            assert isinstance(cur, pl.Expr)
+            cur = cur.is_null().not_()
+
+            if isinstance(expr, type(None)):
+                expr = cur
+            else:
+                expr = expr & cur
+
+        assert isinstance(expr, pl.Expr)
+        return expr
         
     def eval(self, ctx:'Context', **kwargs):
-        return Data()
+        return self.gen_filter(ctx)
--- examples/threading/json.hql
+++ examples/threading/json.hql
@@ -1,7 +1,7 @@
 // shows off some multithreading and multi-tables
 union database('json').macro('all'), database('json').macro('all')
 | project ['@timestamp'], src_ip=toip4(source.ip), src_port=source.port, dest_ip=toip4(destination.ip), dest_port=destination.port
-//| where isnotempty(src_ip)
+| where isnotempty(src_ip)
 | summarize count() by src_ip
 | sort by count_ desc
 | take 10
