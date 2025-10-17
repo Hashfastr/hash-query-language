acf94d4198148de2f6e06259364c51ea3f4e0cf3
Sylvain Jones <sylvainjones@hashfastr.com>
2025-10-16 21:39:49 -0600

    Change port

--- Hql/Apiserver/__init__.py
+++ Hql/Apiserver/__init__.py
@@ -24,7 +24,7 @@ class SigmaRequest(BaseModel):
     plan: bool = False
 
 class Apiserver():
-    def __init__(self, hacengine:'HacEngine', host='127.0.0.1', port=8080):
+    def __init__(self, hacengine:'HacEngine', host='127.0.0.1', port=8081):
         if not can_thread():
             raise hqle.CompilerException('Cannot start the api server as free threading is not supported, use the container?')
 
