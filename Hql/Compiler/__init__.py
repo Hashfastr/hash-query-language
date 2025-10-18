da7590f95f7f61c5cc51e9f7b99b23ee85079e29
Sylvain Jones <sylvainjones@hashfastr.com>
2025-10-18 01:32:13 -0600

    Renames and DSL compiler

--- Hql/Compiler/__init__.py
+++ Hql/Compiler/__init__.py
@@ -1,5 +1,5 @@
 from .BranchDescriptor import BranchDescriptor
 from .InstructionSet import InstructionSet
 from .Compiler import Compiler
-from .HqlCompiler import HqlCompiler
-from .LuceneCompiler import LuceneCompiler
+from .Hql import HqlCompiler
+from .Lucene import LuceneCompiler
